493168073706ac777f57ad2ed73bdb3471d20a89
Serhii Kolodiazhniy <serhii.kolodyazhniy@gmail.com>
2023-04-11 17:02:33 +0300

    deploy config

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/SDTP-Practice/',
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   plugins: [react()],
 });
